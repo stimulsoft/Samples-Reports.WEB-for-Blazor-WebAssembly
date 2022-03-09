8f9edbf77b0b3cc873c1a19bf21d468425688bbe
Ivan Trusov <i.l.trusov@gmail.com>
2022-03-09 16:14:15 +0300

    Add sample for custom function usage
    https://github.com/stimulsoft/Stimulsoft.Reports/issues/5820

---
+++ Adding Custom Function from Referenced Assembly/CustomFunctionsLibrary/CustomFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Stimulsoft.Data.Extensions;
+
+namespace CustomFunctionsLibrary
+{
+    public class CustomFunction
+    {
+		public static decimal MySum(object value)
+		{
+			if (!ListExt.IsList(value))
+				return Stimulsoft.Base.Helpers.StiValueHelper.TryToDecimal(value);
+
+			return Stimulsoft.Data.Functions.Funcs.SkipNulls(ListExt.ToList(value))
+				.TryCastToDecimal()
+				.Sum();
+		}
+	}
+}
